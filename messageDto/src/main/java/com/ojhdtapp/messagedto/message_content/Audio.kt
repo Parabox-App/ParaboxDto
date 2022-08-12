e765802a27478185f7bbf6e14ab377d7f30dc0ac
ojhdt <ojhdtmail@gmail.com>
2022-08-13 00:08:18 +0800

    1.4

---
+++ messageDto/src/main/java/com/ojhdtapp/messagedto/message_content/Audio.kt
@@ -0,0 +1,11 @@
+package com.ojhdtapp.messagedto.message_content
+
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Audio(val url: String, val length: Long, val fileName: String, val fileSize: Long) : MessageContent{
+    val type = MessageContent.AUDIO
+    override fun getContentString(): String {
+        return "[语音]"
+    }
+}
--- messageDto/src/main/java/com/ojhdtapp/messagedto/message_content/Image.kt
+++ messageDto/src/main/java/com/ojhdtapp/messagedto/message_content/Image.kt
@@ -3,7 +3,7 @@ package com.ojhdtapp.messagedto.message_content
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Image(val url: String) : MessageContent {
+data class Image(val url: String, val width: Int, val height: Int) : MessageContent {
     val type = MessageContent.IMAGE
     override fun getContentString(): String {
         return "[图片]"
