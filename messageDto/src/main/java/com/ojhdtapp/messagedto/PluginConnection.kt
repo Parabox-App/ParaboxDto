84cd80dd3af60871095df7aa17111657d026a879
ojhdt <ojhdtmail@gmail.com>
2022-09-19 17:06:36 +0800

    cancel fixed objectId

--- messageDto/src/main/java/com/ojhdtapp/messagedto/PluginConnection.kt
+++ messageDto/src/main/java/com/ojhdtapp/messagedto/PluginConnection.kt
@@ -1,11 +1,14 @@
 package com.ojhdtapp.messagedto
 
 import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class PluginConnection(val connectionType: Int, val objectId: Long, val id: Long) :
+data class PluginConnection(val connectionType: Int, val sendTargetType: Int, val id: Long) :
     Parcelable {
+    @IgnoredOnParcel
+    val objectId = getObjectId(connectionType, sendTargetType, id)
     override fun equals(other: Any?): Boolean {
         return if (other is PluginConnection) {
             objectId == other.objectId
@@ -18,4 +21,11 @@ data class PluginConnection(val connectionType: Int, val objectId: Long, val id:
         result = 31 * result + objectId.hashCode()
         return result
     }
+}
+object SendTargetType {
+    const val USER = 0
+    const val GROUP = 1
+}
+fun getObjectId(connectionType: Int, sendTargetType: Int, id: Long) : Long{
+    return "${connectionType}${sendTargetType}${id}".toLong()
 }
